defd6cf1a616f9a599b7199469bb542a73d1017a
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2016-04-16 20:13:31 +0300

    add heroku support

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar target/myphotogram-0.0.1-SNAPSHOT.jar
--- src/main/java/ru/myphotogram/repository/PhotoRepository.java
+++ src/main/java/ru/myphotogram/repository/PhotoRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.myphotogram.domain.Photo;
-import ru.myphotogram.domain.User;
 
 import java.util.List;
 
@@ -14,15 +13,12 @@ import java.util.List;
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
     @Query("select photo from Photo photo where photo.user.login = ?#{principal.username}")
-    List<Photo> findByUserIsCurrentUser();
+    List<Photo> findPhotos();
 
-    @Query("select photo from Photo photo where photo.user = :user")
-    List<Photo> findPhotos(@Param("user") User user);
+    @Query("select photo from Photo photo where photo.user.login = ?#{principal.username} and photo.year = :year")
+    List<Photo> findPhotos(@Param("year") int year);
 
-    @Query("select photo from Photo photo where photo.user = :user and photo.year = :year")
-    List<Photo> findPhotos(@Param("user") User user, @Param("year") int year);
-
-    @Query("select photo from Photo photo where photo.user = :user and photo.year = :year and month = :month")
-    List<Photo> findPhotos(@Param("user") User user, @Param("year") int year, @Param("month") int month);
+    @Query("select photo from Photo photo where photo.user.login = ?#{principal.username} and photo.year = :year and month = :month")
+    List<Photo> findPhotos(@Param("year") int year, @Param("month") int month);
 
 }
